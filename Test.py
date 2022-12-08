95919cf696be380fbd372ef95c153d99e23b4802
Lucas <lsc25846@gmail.com>
2022-12-08 17:34:42 +0800

    add some file in ignore

--- Test.py
+++ Test.py
@@ -24,64 +24,68 @@ from CNN import CNN_layer
 import joblib
 import time
 
-start = time.time()
+def Test():
+    start = time.time()
 
-SIZE = 64
-validation_images = []
-validation_labels = [] 
-for directory_path in glob.glob("Images/Validation/*"):
-    validation_label = directory_path.split("\\")[-1]
-    for img_path in glob.glob(os.path.join(directory_path, "*.jpg")):
-        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-        img = cv2.resize(img, (SIZE, SIZE))
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        validation_images.append(img)
-        validation_labels.append(validation_label)
-validation_images = np.array(validation_images)
-validation_labels = np.array(validation_labels)
+    SIZE = 64
+    validation_images = []
+    validation_labels = [] 
+    for directory_path in glob.glob("Images/Validation/*"):
+        validation_label = directory_path.split("\\")[-1]
+        for img_path in glob.glob(os.path.join(directory_path, "*.jpg")):
+            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+            img = cv2.resize(img, (SIZE, SIZE))
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            validation_images.append(img)
+            validation_labels.append(validation_label)
+    validation_images = np.array(validation_images)
+    validation_labels = np.array(validation_labels)
 
-#Encode labels from text to integers.
-from sklearn import preprocessing
-le = preprocessing.LabelEncoder()
-le.fit(validation_labels)
-validiation_labels_encoded = le.transform(validation_labels)
+    #Encode labels from text to integers.
+    from sklearn import preprocessing
+    le = preprocessing.LabelEncoder()
+    le.fit(validation_labels)
+    validiation_labels_encoded = le.transform(validation_labels)
 
-#Split data into test and train datasets (already split but assigning to meaningful convention)
-x_validation, y_validation = validation_images, validiation_labels_encoded
+    #Split data into test and train datasets (already split but assigning to meaningful convention)
+    x_validation, y_validation = validation_images, validiation_labels_encoded
 
-###################################################################
-# Normalize pixel values to between 0 and 1
-x_validation = x_validation/255.0
+    ###################################################################
+    # Normalize pixel values to between 0 and 1
+    x_validation = x_validation/255.0
 
 
-#############################
-cnn_model,feature_extractor  = CNN_layer.build_CNN()
+    #############################
+    cnn_model,feature_extractor  = CNN_layer.build_CNN()
 
-#print(cnn_model.summary()) 
+    #print(cnn_model.summary()) 
 
-##########################################
-#Train the CNN model
-cnn_model.load_weights("my_model.h5")
+    ##########################################
+    #Train the CNN model
+    cnn_model.load_weights("my_model.h5")
 
-#RANDOM FOREST
-RF_model = joblib.load("random_forest.joblib")
-#Send test data through same feature extractor process
-X_test_feature = feature_extractor.predict(x_validation)
-#Now predict using the trained RF model. 
-prediction_RF = RF_model.predict(X_test_feature)
-#Inverse le transform to get original label back. 
-prediction_RF = le.inverse_transform(prediction_RF)
+    #RANDOM FOREST
+    RF_model = joblib.load("random_forest.joblib")
+    #Send test data through same feature extractor process
+    X_test_feature = feature_extractor.predict(x_validation)
+    #Now predict using the trained RF model. 
+    prediction_RF = RF_model.predict(X_test_feature)
+    #Inverse le transform to get original label back. 
+    prediction_RF = le.inverse_transform(prediction_RF)
 
-#Print overall accuracy
-from sklearn import metrics
-from sklearn.metrics import confusion_matrix
-print ("Accuracy = ", metrics.accuracy_score(validation_labels, prediction_RF))
+    #Print overall accuracy
+    from sklearn import metrics
+    from sklearn.metrics import confusion_matrix
+    print ("Accuracy = ", metrics.accuracy_score(validation_labels, prediction_RF))
 
-#Confusion Matrix - verify accuracy of each class
-cm = confusion_matrix(validation_labels, prediction_RF)
-#print(cm)
-sns.heatmap(cm, annot=True)
-end = time.time()
-
-print(end - start)
-
+    #Confusion Matrix - verify accuracy of each class
+    cm = confusion_matrix(validation_labels, prediction_RF)
+    #print(cm)
+    sns.heatmap(cm, annot=True)
+    end = time.time()
+    
+    print("execution time: " + str(round((end - start),2)) + " second")
+    return metrics.accuracy_score(validation_labels, prediction_RF)
+    
+if __name__ == '__main__':    
+    print(Test())
